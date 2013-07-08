d29908926e8c1c026bd3f723bbe11827471d8da0
heartsentwined <heartsentwined.me@gmail.com>
2013-07-08 14:21:06 +0800

    rails engine setup

---
+++ lib/epf-rails.rb
@@ -0,0 +1,15 @@
+require 'rails'
+require 'epf/source'
+require 'epf/rails/version'
+require 'epf/rails/engine'
+
+module Epf
+  module Rails
+    class Railtie < ::Rails::Railtie
+      initializer 'epf.setup-vendor', :group => :all do |app|
+        app.assets.append_path \
+          File.expand_path('../', ::Epf::Source.bundled_path)
+      end
+    end
+  end
+end
---
+++ lib/epf/rails/engine.rb
@@ -0,0 +1,6 @@
+module Epf
+  module Rails
+    class Engine < ::Rails::Engine
+    end
+  end
+end
